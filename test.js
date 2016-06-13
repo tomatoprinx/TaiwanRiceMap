f8ac9368625c8314f2ac9ea23ba469fa025800c0
TomatoPrince <benson910201@Gmail.com>
2016-06-13 18:51:30 +0800

    show

--- test.js
+++ test.js
@@ -1,37 +1,55 @@
-var width = 800,
-    height = 600;
+$(document).ready(function(){
+drawTaiwan()
 
-var svg = d3.select("body").append("svg")
-    // .attr("class", "svgback")
-    .attr("width", width)
-    .attr("height", height);
-var projection = d3.geo.mercator()
-    .center([121,24])
-    .scale(6000);
-
-var path = d3.geo.path()
-    .projection(projection);
-d3.json("county.json", function(error, topology) {
-    var g = svg.append("g");
-    
-    // 縣市/行政區界線
-    d3.select("svg").append("path").datum(
-            topojson.mesh(topology,
-                    topology.objects["county"], function(a,
-                            b) {
-                        return a !== b;
-                    })).attr("d", path).attr("class","subunit-boundary");
-    
-    d3.select("g").selectAll("path")
-          .data(topojson.feature(topology, topology.objects.county).features)
-          .enter()
-          .append("path")
-          .attr("d", path)
-          .attr({
-                d : path,
-                name : function(d) {
-                    return d.properties["C_Name"];
-                },
-                fill : '#55AA00'
-        });
 });
+
+function drawTaiwan(){
+    var type='county';
+    var scale = 12500;
+    var Cname = 'C_Name';
+
+    d3.json(type+".js", function(topodata) {
+     // features = topojson.feature(topodata, topodata.objects.country).features;
+      // 這裡要注意的是 topodata.objects["county"] 中的 "county" 為原本 shp 的檔名
+  this.topodata = topodata;
+
+  features = topojson.feature(topodata, topodata.objects[type]).features;
+  
+    path = d3.geo.path().projection( // 路徑產生器
+    d3.geo.mercator().center([121,24]).scale(scale) // 座標變換函式
+  );
+
+    
+  d3.select("#pathCanvas").selectAll("path").data(features).enter().append("path").attr({
+    d: path,
+    name: function(d){
+      return d.properties[Cname];
+    },
+    fill:'rgba(0,0,0,.5)'
+ });
+
+  d3.select("#pathCanvas").append("path")         //縣市/行政區界線
+    .datum(topojson.mesh(topodata, topodata.objects[type], function(a, b) { return a !== b ; }))
+    .attr("d", path)
+    .attr("id", "county-boundary");
+
+d3.select("svg").selectAll("path").on("mouseenter", function() {          //title div 顯示滑鼠所指向的縣市/行政區
+      $('#panel').css("display","inline");
+      $(this).attr("fill", 'rgba(255,255,255,.5)');
+      $('#title').html($(this).attr( "name" ));
+      $('#panel').css({"height": "20px","width": "50px"});
+    }).on("mouseout", function() {
+      $(this).attr("fill", 'rgba(0,0,0,.5)');
+    });   
+
+    $("path").mouseover(function(){                   //info 區塊跟隨滑鼠移動
+    $("path").mousemove( function(e) {
+     mouseX = e.pageX; 
+     mouseY = e.pageY;
+    });  
+    $('#panel').css({'top':mouseY,'left':mouseX}).fadeIn('slow');
+  });
+
+});
+
+}
